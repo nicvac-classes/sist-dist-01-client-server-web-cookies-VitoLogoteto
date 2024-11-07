1e8071d2d0a53b11ab349ba47750fff98946f759
VitLog <vito.logoteto@marconibari.edu.it>
2024-11-07 18:42:21 +0000

    CONSEGNA

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 
-// Configurazione EJS
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
-    res.render('form');
+    const name = req.cookies.name;
+    if (name) {
+
+        res.render('greet', { message: 'Bentornato', name: name });
+    } else {
+
+        res.render('form');
+    }
 });
+
 
 app.post('/greet', (req, res) => {
     const name = req.body.name;
-    res.render('greet', { name: name });
+    res.cookie('name', name, { maxAge: 24 * 60 * 60 * 1000 });
+    res.render('greet', { message: 'Benvenuto', name: name });
 });
+
+app.post('/logout', (req, res) => {
+    res.clearCookie('name');
+    res.redirect('/');
+});
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
